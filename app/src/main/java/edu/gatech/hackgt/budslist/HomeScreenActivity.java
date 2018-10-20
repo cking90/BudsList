aa2af2896ab628f017c3baf00039c5280c4a55ba
cking90 <cking90@gatech.edu>
2018-10-20 16:59:33 -0400

    Start initialization of default data on opening activity

--- app/src/main/java/edu/gatech/hackgt/budslist/HomeScreenActivity.java
+++ app/src/main/java/edu/gatech/hackgt/budslist/HomeScreenActivity.java
@@ -13,10 +13,12 @@ import edu.gatech.hackgt.budslist.models.Model;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
+    Model model;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        model = Model.getInstance();
     }
 
     public void onClickLogin(View view) {
@@ -29,5 +31,12 @@ public class HomeScreenActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
+    public void initDefaultData() {
+        String[] names = {"Matt", "Saira", "Neha", "Neha"};
+        for (String name : names) {
+            String email = String.format("%s@gmail.com", name);
+            String password = name.toLowerCase();
+//            model.addUser("");
+        }
+    }
 }
