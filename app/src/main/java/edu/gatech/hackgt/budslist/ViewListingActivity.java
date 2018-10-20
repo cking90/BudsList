c30f3efe21f06f2bd38e291fe904520e43ea9638
nehadesh <neha.n.deshpande@gmail.com>
2018-10-20 19:19:22 -0400

    Add: ViewListingActivity

--- app/src/main/java/edu/gatech/hackgt/budslist/ViewListingActivity.java
+++ app/src/main/java/edu/gatech/hackgt/budslist/ViewListingActivity.java
@@ -1,13 +1,32 @@
 package edu.gatech.hackgt.budslist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ViewListingActivity extends AppCompatActivity {
 
+    String userEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_listing);
+
+        Bundle extras = getIntent().getExtras();
+        userEmail = extras.getString("user_email");
+    }
+
+    public void onClickBack(View view) {
+        Intent intent = new Intent(this, MyListingsActivity.class);
+        intent.putExtra("user_email", userEmail);
+        startActivity(intent);
+    }
+
+    public void onClickDelete(View view) {
+        Intent intent = new Intent(this, MyListingsActivity.class);
+        //TODO- add delete functionality
+        intent.putExtra("user_email", userEmail);
+        startActivity(intent);
     }
 }
