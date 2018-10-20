c5e602ff3fd5f6bded35b32f4e38c721cdf3c7bd
nehadesh <neha.n.deshpande@gmail.com>
2018-10-20 16:31:49 -0400

    Merge branch 'master' of https://github.com/cking90/BudsList
    # Conflicts:
    #	app/src/main/java/edu/gatech/hackgt/budslist/models/Book.java

--- app/src/main/java/edu/gatech/hackgt/budslist/CreateListingActivity.java
+++ app/src/main/java/edu/gatech/hackgt/budslist/CreateListingActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import edu.gatech.hackgt.budslist.models.Binding;
+import edu.gatech.hackgt.budslist.models.Department;
 
 public class CreateListingActivity extends AppCompatActivity {
 
@@ -15,6 +17,8 @@ public class CreateListingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_listing);
 
+        Spinner department = (Spinner)findViewById(R.id.spinner_department_id);
+        department.setAdapter(new ArrayAdapter<Department>(this, android.R.layout.simple_spinner_item, Department.values()));
         Spinner binding = (Spinner)findViewById(R.id.spinner_binding_id);
         binding.setAdapter(new ArrayAdapter<Binding>(this, android.R.layout.simple_spinner_item, Binding.values()));
     }
@@ -24,6 +28,14 @@ public class CreateListingActivity extends AppCompatActivity {
         String isbn = isbn_box.getText().toString();
         EditText price_box = (EditText)findViewById(R.id.editText_price_id);
         String price = price_box.getText().toString();
+
+        Spinner binding = (Spinner)findViewById(R.id.spinner_binding_id);
+        String selected_binding = binding.getSelectedItem().toString();
+        Spinner department = (Spinner)findViewById(R.id.spinner_department_id);
+        String selected_department = department.getSelectedItem().toString();
+
+        EditText num_box = (EditText)findViewById(R.id.editText_courseNum_id);
+        String course_num = num_box.getText().toString();
 
         Intent intent = new Intent(this, MyListingsActivity.class);
         startActivity(intent);
