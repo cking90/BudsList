c77d27abd7d0aec401697669ad8b3be00e0c2fed
cking90 <cking90@gatech.edu>
2018-10-20 14:18:06 -0400

    Change user phone number to String

--- app/src/main/java/edu/gatech/hackgt/budslist/models/Model.java
+++ app/src/main/java/edu/gatech/hackgt/budslist/models/Model.java
@@ -15,6 +15,8 @@ public class Model {
 
     private final List<Request> requests = new ArrayList<>();
 
+    private final List<Book> books = new ArrayList<>();
+
     /**
      * Singleton pattern
      */
@@ -36,23 +38,43 @@ public class Model {
      */
     public static Model getInstance() { return instance; }
 
-    public void addUser(String email, String password, String name, long phone_number) {
+    public void addUser(String email, String password, String name, String phone_number) {
         User user = new User(email, password, name, phone_number);
         users.put(email, user);
     }
     public void removeUser(String email) {
         users.remove(email);
     }
+
     public Map<String, User> getUsers() {
         return users;
     }
+
     public User getUserByEmail(String email) {
         return users.get(email);
     }
 
+
+
+
     public void addRequest(User buyer, User seller, Book book) {
         requests.add(new Request(buyer, seller, book));
     }
+    public void removeRequest(Request request) {
+        requests.remove(request);
+    }
+    public Request getRequest(String isbn, String sellerEmail, String buyerEmail) {
+        for (Request r : requests) {
+            if (r.getBook().getIsbn().equals(isbn) && r.getBuyer().getEmail().equals(buyerEmail)
+                    && r.getSeller().getEmail().equals(sellerEmail)) {
+                return r;
+            }
+        }
+        return null;
+    }
+
+
+
 
 
 
