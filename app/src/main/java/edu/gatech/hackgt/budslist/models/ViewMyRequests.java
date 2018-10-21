d751c2d40b33e73f997687c27bb330005e93113c
Saira <sairapoonnen@gmail.com>
2018-10-21 01:56:07 -0400

    add view my requests activity

---
+++ app/src/main/java/edu/gatech/hackgt/budslist/models/ViewMyRequests.java
@@ -0,0 +1,15 @@
+package edu.gatech.hackgt.budslist.models;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.hackgt.budslist.R;
+
+public class ViewMyRequests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_my_requests);
+    }
+}
