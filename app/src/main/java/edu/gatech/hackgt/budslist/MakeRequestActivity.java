a6822eaddad778894c45584616730a2da882ec25
Saira <sairapoonnen@gmail.com>
2018-10-20 23:31:17 -0400

    add instance fields to requests activity

--- app/src/main/java/edu/gatech/hackgt/budslist/MakeRequestActivity.java
+++ app/src/main/java/edu/gatech/hackgt/budslist/MakeRequestActivity.java
@@ -4,8 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MakeRequestActivity extends AppCompatActivity {
+
+    TextView textView_title;
+    TextView textView_isbn;
+    TextView textView_price;
+    TextView textView_author;
+    TextView textView_binding;
+    TextView textView_course;
+    TextView textView_seller;
 
 
     String userEmail;
@@ -16,6 +25,15 @@ public class MakeRequestActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         userEmail = extras.getString("user_email");
+
+
+        textView_title = findViewById(R.id.textView_title);
+        textView_isbn = findViewById(R.id.textView_isbn);
+        textView_price = findViewById(R.id.textView_price);
+        textView_author = findViewById(R.id.textView_author);
+        textView_binding = findViewById(R.id.textView_binding);
+        textView_course = findViewById(R.id.textView_course);
+        textView_seller = findViewById(R.id.textView_seller);
     }
 
     public void onClickBack(View view) {
