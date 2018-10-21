e3f23f671c49ead2fbb9177d527ec3b020328de7
cking90 <cking90@gatech.edu>
2018-10-21 02:26:15 -0400

    Push work on view requests page

--- app/src/main/java/edu/gatech/hackgt/budslist/ViewMyRequestsActivity.java
+++ app/src/main/java/edu/gatech/hackgt/budslist/ViewMyRequestsActivity.java
@@ -2,13 +2,20 @@ package edu.gatech.hackgt.budslist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import edu.gatech.hackgt.budslist.R;
+import edu.gatech.hackgt.budslist.models.Book;
 import edu.gatech.hackgt.budslist.models.Model;
 
 public class ViewMyRequestsActivity extends AppCompatActivity {
     Model model;
     String userEmail;
+
+    Spinner outgoing;
+    Spinner incoming;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +24,11 @@ public class ViewMyRequestsActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         userEmail = extras.getString("user_email");
         model = Model.getInstance();
+
+        Spinner outgoing = (Spinner)findViewById(R.id.spinner_outgoing);
+        outgoing.setAdapter(new ArrayAdapter<Book>(this, android.R.layout.simple_spinner_item, ));
+
+        Spinner userType = (Spinner)findViewById(R.id.myListingsSpinner);
+        userType.setAdapter(new ArrayAdapter<Book>(this, android.R.layout.simple_spinner_item, listings));
     }
 }
